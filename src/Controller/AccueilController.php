46ac5017e69f679798419f4ea4cd8d2bb116af30
delah <delah@DESKTOP-LF99LE6>
2024-02-15 19:32:25 +0100

    Phase 2: premier test "Hello World!"

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AccueilController
+ *
+ * @author delah
+ */
+class AccueilController {
+
+    /**
+     * @Route("/", name="acceuil")
+     * @return Response
+     */
+    public function index(): Response{
+        return new Response('Hello World');
+    }
+}
