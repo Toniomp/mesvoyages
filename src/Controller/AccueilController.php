201e95cd0056659e72338a083334199b78fa6560
delah <delah@DESKTOP-LF99LE6>
2024-02-16 17:38:02 +0100

    Phase 3: utilisation de Twig

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,6 +7,7 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,13 +16,13 @@ use Symfony\Component\Routing\Annotation\Route;
  *
  * @author delah
  */
-class AccueilController {
+class AccueilController extends AbstractController {
 
     /**
      * @Route("/", name="acceuil")
      * @return Response
      */
     public function index(): Response{
-        return new Response('Hello World');
+        return $this->render("pages/accueil.html.twig");
     }
 }
